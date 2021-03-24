8977f33fe288a6229fabbce8825049ec096f3c55
Dzevad <ynwa886@gmail.com>
2021-03-24 20:05:08 +0100

    Destructuring Assignment to Extract Values

---
+++ ex02/src/index.js
@@ -0,0 +1,17 @@
+const MY_CAR = {
+    wheels: 4,
+    cylinders: 4,
+    size: 2.2
+};
+
+function main(myCar) {
+
+    const cylinders = MY_CAR.cylinders;
+    const size = MY_CAR.size;
+
+    return { cylinders, size };
+}
+
+console.log(main(MY_CAR));
+
+module.exports = main;
